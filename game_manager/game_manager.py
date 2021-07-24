ec4f86f4cb67398deaf81ce5a537e0588f361226
Ishii Daichi <ishidai.job@gmail.com>
2021-07-24 22:56:06 +0000

    version DDQN

--- game_manager/game_manager.py
+++ game_manager/game_manager.py
@@ -14,6 +14,253 @@ import time
 import json
 import pprint
 
+############# add DQN #################
+
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+import pprint
+import random
+
+import random
+import torch
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from board_manager import BOARD_DATA
+
+BATCH_SIZE = 32
+CAPACITY = 10000
+GAMMA = 0.99  # 時間割引率
+MAX_STEPS = 200  # 1試行のstep数
+NUM_EPISODES = 500  # 最大試行回数
+
+from collections import namedtuple
+
+Transition = namedtuple(
+    'Transition', ('state', 'action', 'next_state', 'reward'))
+
+class ReplayMemory:
+
+    def __init__(self, CAPACITY):
+        self.capacity = CAPACITY  # メモリの最大長さ
+        self.memory = []  # 経験を保存する変数
+        self.index = 0  # 保存するindexを示す変数
+
+    def push(self, state, action, state_next, reward):
+        '''transition = (state, action, state_next, reward)をメモリに保存する'''
+
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)  # メモリが満タンでないときは足す
+
+        # namedtupleのTransitionを使用し、値とフィールド名をペアにして保存します
+        self.memory[self.index] = Transition(state, action, state_next, reward)
+
+        self.index = (self.index + 1) % self.capacity  # 保存するindexを1つずらす
+
+    def sample(self, batch_size):
+        '''batch_size分だけ、ランダムに保存内容を取り出す'''
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        '''関数lenに対して、現在の変数memoryの長さを返す'''
+        return len(self.memory)
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+
+    def __init__(self, n_in, n_mid, n_out):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(n_in, n_mid)
+        self.fc2 = nn.Linear(n_mid, n_mid)
+        self.fc3 = nn.Linear(n_mid, n_out)
+
+    def forward(self, x):
+        h1 = F.relu(self.fc1(x))
+        h2 = F.relu(self.fc2(h1))
+        output = self.fc3(h2)
+        return output
+
+class Brain:
+    def __init__(self, num_states, num_actions):
+        self.num_actions = num_actions  # CartPoleの行動（右に左に押す）の2を取得
+
+        # 経験を記憶するメモリオブジェクトを生成
+        self.memory = ReplayMemory(CAPACITY)
+
+        # ニューラルネットワークを構築
+        n_in, n_mid, n_out = num_states, 32, num_actions
+        self.main_q_network = Net(n_in, n_mid, n_out)  # Netクラスを使用
+        self.target_q_network = Net(n_in, n_mid, n_out)  # Netクラスを使用
+        print(self.main_q_network)  # ネットワークの形を出力
+
+        # 最適化手法の設定
+        self.optimizer = optim.Adam(
+            self.main_q_network.parameters(), lr=0.0001)
+
+    def replay(self):
+        '''Experience Replayでネットワークの結合パラメータを学習'''
+
+        # 1. メモリサイズの確認
+        if len(self.memory) < BATCH_SIZE:
+            return
+
+        # 2. ミニバッチの作成
+        self.batch, self.state_batch, self.action_batch, self.reward_batch, self.non_final_next_states = self.make_minibatch()
+
+        # 3. 教師信号となるQ(s_t, a_t)値を求める
+        self.expected_state_action_values = self.get_expected_state_action_values()
+
+        # 4. 結合パラメータの更新
+        self.update_main_q_network()
+
+    def decide_action(self, state, episode):
+        '''現在の状態に応じて、行動を決定する'''
+        # ε-greedy法で徐々に最適行動のみを採用する
+        # epsilon = 0.5 * (1 / (episode + 1))
+        epsilon = 1.0 - episode/100000
+
+        print("epsilon : {0}".format(epsilon))
+
+        if epsilon <= np.random.uniform(0, 1):
+            self.main_q_network.eval()  # ネットワークを推論モードに切り替える
+            with torch.no_grad():
+                action = self.main_q_network(state).max(1)[1].view(1, 1)
+            # ネットワークの出力の最大値のindexを取り出します = max(1)[1]
+            # .view(1,1)は[torch.LongTensor of size 1]　を size 1x1 に変換します
+
+        else:
+            # 0,1の行動をランダムに返す
+            action = torch.LongTensor(
+                [[random.randrange(self.num_actions)]])  # 0,1の行動をランダムに返す
+            # actionは[torch.LongTensor of size 1x1]の形になります
+
+        return action
+
+    def make_minibatch(self):
+        '''2. ミニバッチの作成'''
+
+        # 2.1 メモリからミニバッチ分のデータを取り出す
+        transitions = self.memory.sample(BATCH_SIZE)
+
+        # 2.2 各変数をミニバッチに対応する形に変形
+        # transitionsは1stepごとの(state, action, state_next, reward)が、BATCH_SIZE分格納されている
+        # つまり、(state, action, state_next, reward)×BATCH_SIZE
+        # これをミニバッチにしたい。つまり
+        # (state×BATCH_SIZE, action×BATCH_SIZE, state_next×BATCH_SIZE, reward×BATCH_SIZE)にする
+        batch = Transition(*zip(*transitions))
+
+        # 2.3 各変数の要素をミニバッチに対応する形に変形し、ネットワークで扱えるようVariableにする
+        # 例えばstateの場合、[torch.FloatTensor of size 1x4]がBATCH_SIZE分並んでいるのですが、
+        # それを torch.FloatTensor of size BATCH_SIZEx4 に変換します
+        # 状態、行動、報酬、non_finalの状態のミニバッチのVariableを作成
+        # catはConcatenates（結合）のことです。
+        state_batch = torch.cat(batch.state)
+        action_batch = torch.cat(batch.action)
+        reward_batch = torch.cat(batch.reward)
+        non_final_next_states = torch.cat([s for s in batch.next_state
+                                           if s is not None])
+
+        return batch, state_batch, action_batch, reward_batch, non_final_next_states
+
+    def get_expected_state_action_values(self):
+        '''3. 教師信号となるQ(s_t, a_t)値を求める'''
+
+        # 3.1 ネットワークを推論モードに切り替える
+        self.main_q_network.eval()
+        self.target_q_network.eval()
+
+        # 3.2 ネットワークが出力したQ(s_t, a_t)を求める
+        # self.model(state_batch)は、右左の両方のQ値を出力しており
+        # [torch.FloatTensor of size BATCH_SIZEx2]になっている。
+        # ここから実行したアクションa_tに対応するQ値を求めるため、action_batchで行った行動a_tが右か左かのindexを求め
+        # それに対応するQ値をgatherでひっぱり出す。
+        self.state_action_values = self.main_q_network(
+            self.state_batch).gather(1, self.action_batch)
+
+        # 3.3 max{Q(s_t+1, a)}値を求める。ただし次の状態があるかに注意。
+
+        # cartpoleがdoneになっておらず、next_stateがあるかをチェックするインデックスマスクを作成
+        non_final_mask = torch.ByteTensor(tuple(map(lambda s: s is not None,
+                                                    self.batch.next_state)))
+        # まずは全部0にしておく
+        next_state_values = torch.zeros(BATCH_SIZE)
+
+        a_m = torch.zeros(BATCH_SIZE).type(torch.LongTensor)
+
+        # 次の状態での最大Q値の行動a_mをMain Q-Networkから求める
+        # 最後の[1]で行動に対応したindexが返る
+        a_m[non_final_mask] = self.main_q_network(
+            self.non_final_next_states).detach().max(1)[1]
+
+        # 次の状態があるものだけにフィルターし、size 32を32×1へ
+        a_m_non_final_next_states = a_m[non_final_mask].view(-1, 1)
+
+        # 次の状態があるindexの、行動a_mのQ値をtarget Q-Networkから求める
+        # detach()で取り出す
+        # squeeze()でsize[minibatch×1]を[minibatch]に。
+        next_state_values[non_final_mask] = self.target_q_network(
+            self.non_final_next_states).gather(1, a_m_non_final_next_states).detach().squeeze()
+
+        # 3.4 教師となるQ(s_t, a_t)値を、Q学習の式から求める
+        expected_state_action_values = self.reward_batch + GAMMA * next_state_values
+
+        return expected_state_action_values
+
+    def update_main_q_network(self):
+        '''4. 結合パラメータの更新'''
+
+        # 4.1 ネットワークを訓練モードに切り替える
+        self.main_q_network.train()
+
+        # 4.2 損失関数を計算する（smooth_l1_lossはHuberloss）
+        # expected_state_action_valuesは
+        # sizeが[minbatch]になっているので、unsqueezeで[minibatch x 1]へ
+        loss = F.smooth_l1_loss(self.state_action_values,
+                                self.expected_state_action_values.unsqueeze(1))
+
+        print("loss : {0}".format(loss))
+
+        # 4.3 結合パラメータを更新する
+        self.optimizer.zero_grad()  # 勾配をリセット
+        loss.backward()  # バックプロパゲーションを計算
+        self.optimizer.step()  # 結合パラメータを更新
+
+    def update_target_q_network(self):  # DDQNで追加
+        '''Target Q-NetworkをMainと同じにする'''
+        self.target_q_network.load_state_dict(self.main_q_network.state_dict())
+
+class Agent:
+    def __init__(self, num_states, num_actions):
+        '''課題の状態と行動の数を設定する'''
+        self.brain = Brain(num_states, num_actions)  # エージェントが行動を決定するための頭脳を生成
+
+    def update_q_function(self):
+        '''Q関数を更新する'''
+        self.brain.replay()
+
+    def get_action(self, state, episode):
+        '''行動を決定する'''
+        action = self.brain.decide_action(state, episode)
+        return action
+
+    def memorize(self, state, action, state_next, reward):
+        '''memoryオブジェクトに、state, action, state_next, rewardの内容を保存する'''
+        self.brain.memory.push(state, action, state_next, reward)
+
+    def update_target_q_function(self):
+        '''Target Q-NetworkをMain Q-Networkと同じに更新'''
+        self.brain.update_target_q_network()
+
+#######################################
+
 def get_option(game_time, manual, use_sample, drop_speed, random_seed, obstacle_height, obstacle_probability, resultlogjson):
     argparser = ArgumentParser()
     argparser.add_argument('--game_time', type=int,
@@ -56,6 +303,20 @@ class Game_Manager(QMainWindow):
 
         self.episode = 0
         self.step = 0
+
+        self.num_states = 220
+        self.num_actions = 40
+
+        self.agent = Agent(self.num_states, self.num_actions)
+
+        self.done = False
+
+        self.action = None
+        self.observation = None
+        self.state = None
+        self.observation_next = None
+        self.state_next = None
+        self.reward = None
 
         self.isStarted = False
         self.isPaused = False
@@ -181,63 +442,10 @@ class Game_Manager(QMainWindow):
         BOARD_DATA.clear()
         BOARD_DATA.createNewPiece()
 
-        GameStatus = self.getGameStatus()
-        BLOCK_CONTROLLER.reset_state(GameStatus)
-
     def updateWindow(self):
         self.tboard.updateData()
         self.sidePanel.updateData()
         self.update()
-
-    def move_block(self):
-        if self.nextMove:
-            # shape direction operation
-            next_x = self.nextMove["strategy"]["x"]
-            next_y_moveblocknum = self.nextMove["strategy"]["y_moveblocknum"]
-            y_operation = self.nextMove["strategy"]["y_operation"]
-            next_direction = self.nextMove["strategy"]["direction"]
-            k = 0
-            while BOARD_DATA.currentDirection != next_direction and k < 4:
-                ret = BOARD_DATA.rotateRight()
-                if ret == False:
-                    print("cannot rotateRight")
-                    break
-                k += 1
-            # x operation
-            k = 0
-            while BOARD_DATA.currentX != next_x and k < 5:
-                if BOARD_DATA.currentX > next_x:
-                    ret = BOARD_DATA.moveLeft()
-                    if ret == False:
-                        print("cannot moveLeft")
-                        break
-                elif BOARD_DATA.currentX < next_x:
-                    ret = BOARD_DATA.moveRight()
-                    if ret == False:
-                        print("cannot moveRight")
-                        break
-                k += 1
-
-        # dropdown/movedown lines
-        dropdownlines = 0
-        removedlines = 0
-        if y_operation == 1: # dropdown
-            removedlines, dropdownlines = BOARD_DATA.dropDown()
-        else: # movedown, with next_y_moveblocknum lines
-            k = 0
-            while True:
-                removedlines, movedownlines = BOARD_DATA.moveDown()
-                if movedownlines < 1:
-                    # if already dropped
-                    break
-                k += 1
-                if k >= next_y_moveblocknum:
-                    # if already movedown next_y_moveblocknum block
-                    break
-
-        self.UpdateScore(removedlines, dropdownlines)
-
-        return removedlines
 
     def timerEvent(self, event):
         # callback function for user control
@@ -264,13 +472,17 @@ class Game_Manager(QMainWindow):
                 # get nextMove from GameController
                 GameStatus = self.getGameStatus()
 
-                # step count up
-                self.step += 1
+                self.done = False
 
-                # if self.use_sample == "y":
-                self.nextMove = BLOCK_CONTROLLER.GetNextMove(nextMove, GameStatus)
-                # else:
-                #     self.nextMove = BLOCK_CONTROLLER.GetNextMove(nextMove, GameStatus)
+                self.observation = GameStatus["field_info"]["withblock"]
+                self.state = np.array(self.observation)
+                self.state = torch.from_numpy(self.state).type(torch.FloatTensor)  # numpy変数をPyTorchのテンソルに変換
+                self.state = torch.unsqueeze(self.state, 0)
+                self.state = self.state / 7.0
+
+                self.action = self.agent.get_action(self.state, self.episode)
+
+                self.nextMove = BLOCK_CONTROLLER.GetNextMove(GameStatus, nextMove, self.action.item())
 
                 if self.manual in ("y", "g"):
                     # ignore nextMove, for manual controll
@@ -279,20 +491,108 @@ class Game_Manager(QMainWindow):
                     self.nextMove["strategy"]["y_operation"] = 0
                     self.nextMove["strategy"]["direction"] = BOARD_DATA.currentDirection
 
-            remove_lines = self.move_block()
-            BLOCK_CONTROLLER.train(GameStatus, remove_lines)
+            if self.nextMove:
+                # shape direction operation
+                next_x = self.nextMove["strategy"]["x"]
+                next_y_moveblocknum = self.nextMove["strategy"]["y_moveblocknum"]
+                y_operation = self.nextMove["strategy"]["y_operation"]
+                next_direction = self.nextMove["strategy"]["direction"]
+                k = 0
+                while BOARD_DATA.currentDirection != next_direction and k < 4:
+                    ret = BOARD_DATA.rotateRight()
+                    if ret == False:
+                        print("cannot rotateRight")
+                        break
+                    k += 1
+                # x operation
+                k = 0
+                while BOARD_DATA.currentX != next_x and k < 5:
+                    if BOARD_DATA.currentX > next_x:
+                        ret = BOARD_DATA.moveLeft()
+                        if ret == False:
+                            print("cannot moveLeft")
+                            break
+                    elif BOARD_DATA.currentX < next_x:
+                        ret = BOARD_DATA.moveRight()
+                        if ret == False:
+                            print("cannot moveRight")
+                            break
+                    k += 1
 
-            # check reset field
+            # dropdown/movedown lines
+            dropdownlines = 0
+            removedlines = 0
+            if y_operation == 1: # dropdown
+                removedlines, dropdownlines = BOARD_DATA.dropDown()
+            else: # movedown, with next_y_moveblocknum lines
+                k = 0
+                while True:
+                    removedlines, movedownlines = BOARD_DATA.moveDown()
+                    if movedownlines < 1:
+                        # if already dropped
+                        break
+                    k += 1
+                    if k >= next_y_moveblocknum:
+                        # if already movedown next_y_moveblocknum block
+                        break
+
+            self.UpdateScore(removedlines, dropdownlines)
+
+            GameStatus = self.getGameStatus()
+            self.observation_next = GameStatus["field_info"]["backboard"]
+
+            self.reward = 0
+
             if BOARD_DATA.currentY < 1:
-                # if Piece cannot movedown and stack, reset field
-                print("reset field.")
-                self.resetfield()
+                self.state_next = None
+                print("reset episode")
+                self.reset_episode()
+                self.reward = torch.FloatTensor([Game_Manager.GAMEOVER_SCORE])
+                self.done = True
+
+            if removedlines > 0:
+                self.state_next = np.array(self.observation_next)
+                self.state_next = torch.from_numpy(self.state_next).type(torch.FloatTensor)
+                self.state_next = torch.unsqueeze(self.state_next, 0)
+                self.state_next = self.state_next / 7.0
+
+                if removedlines == 1:
+                    linescore = Game_Manager.LINE_SCORE_1
+                elif removedlines == 2:
+                    linescore = Game_Manager.LINE_SCORE_2
+                elif removedlines == 3:
+                    linescore = Game_Manager.LINE_SCORE_3
+                elif removedlines == 4:
+                    linescore = Game_Manager.LINE_SCORE_4
+
+                self.reward = torch.FloatTensor([linescore])
+
+            else:
+                self.state_next = np.array(self.observation_next)
+                self.state_next = torch.from_numpy(self.state_next).type(torch.FloatTensor)
+                self.state_next = torch.unsqueeze(self.state_next, 0)
+                self.state_next = self.state_next / 7.0
+                self.reward = torch.FloatTensor([0.0])
+
+            # メモリに経験を追加
+            self.agent.memorize(self.state, self.action, self.state_next, self.reward)
+
+            # Experience ReplayでQ関数を更新する
+            self.agent.update_q_function()
+
+            # 終了時の処理
+            if self.done:
+                # DDQNで追加、2試行に1度、Target Q-NetworkをMainと同じにコピーする
+                if(self.episode % 2 == 0):
+                    self.agent.update_target_q_function()
 
             # init nextMove
             self.nextMove = None
 
             # update window
             self.updateWindow()
+
+            self.step += 1
 
         else:
             super(Game_Manager, self).timerEvent(event)
